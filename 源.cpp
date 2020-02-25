5b0654358c5ac9578bcdf59021026cc3cd8b5331
Ruixin Li <l94212kart@vip.qq.com>
2020-02-25 23:47:44 +0800

    4 ms	12.4 MB
    https://leetcode-cn.com/problems/two-sum/

--- 源.cpp
+++ 源.cpp
@@ -1,21 +1,32 @@
 #include <iostream>
 #include <string>
 #include <vector>
-#include <list>
-#include <deque>
-#include <array>
-#include <forward_list>
+#include <map>
+#include <set>
+#include <unordered_map>
+#include <unordered_set>
 #include <algorithm>
+#include <random>
 using namespace std;
+
+vector<int> twoSum(vector<int>& nums, int target) {
+	unordered_map<int, int> data;
+	int i;
+	for (i = 0; i != nums.size(); ++i) {
+		if (data.find(target - nums[i]) != cend(data)) break;
+		else data.insert(make_pair(nums[i],i));
+	}
+	if (i < nums.size())
+		return vector<int>{data[target - nums[i]], i};
+	else
+		return vector<int>();
+}
 
 int main()
 {
-	std::numeric_limits<int>::epsilon();
-	cin.sync_with_stdio(false);
-	cout.sync_with_stdio(false);
-	vector<string> svec{ "123.0","234.4","4323.2" };
-	double sum = 0.0;
-	for_each(begin(svec), end(svec), [&sum](string& i) {sum += stod(i); });
-	cout << sum;
+	vector<int> dataSource = { 2,11,14,99,4 };
+	int target = 16;
+	auto answer = twoSum(dataSource, target);
+	for (auto i : answer) cout << i << "|";
 	return 0;
 }
