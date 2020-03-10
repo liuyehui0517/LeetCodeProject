1daa5854e51dc8aee2a2d3d241d648f4601b2587
Ruixin Li <l94212kart@vip.qq.com>
2020-03-10 19:18:28 +0800

    nothing changed. just for fun

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # LeetCodeProject
 LeetCode Solutions
+
 [![Build Status](https://travis-ci.com/Msopopop/LeetCodeProject.svg?branch=master)](https://travis-ci.com/Msopopop/LeetCodeProject)
 [![codecov](https://codecov.io/gh/Msopopop/LeetCodeProject/branch/master/graph/badge.svg)](https://codecov.io/gh/Msopopop/LeetCodeProject)
 
--- main.cpp
+++ main.cpp
@@ -1,30 +1,21 @@
 #include <iostream>
-#include <string>
 #include <vector>
-using namespace std;
 
-#ifdef _MSC_VER
+using namespace std;
+#if (defined _MSC_VER) && (defined _DEBUG)
 #define _CRTDBG_MAP_ALLOC
 #include <cstdlib> 
 #include <crtdbg.h>
-#ifdef _DEBUG
-	#define DBG_NEW new ( _NORMAL_BLOCK , __FILE__ , __LINE__ )
-#else
-	#define DBG_NEW new
-#endif
-#endif
-
-#ifdef __GNUC__
-#pragma GCC optimize(2)
-#pragma G++ optimize(2) 
 #endif
 
 int main()
 {
-	#ifdef _MSC_VER
+#if (defined _MSC_VER) && (defined _DEBUG) 
+#define DBG_NEW new ( _NORMAL_BLOCK , __FILE__ , __LINE__ )
 	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
-	#endif
-	cin.sync_with_stdio(false);
-	cout.sync_with_stdio(false);
+#else
+#define DBG_NEW new
+#endif
+	ios::sync_with_stdio(false);
 	return 0;
 }
